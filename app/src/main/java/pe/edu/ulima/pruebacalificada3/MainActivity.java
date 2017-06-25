818f41e86b2abc5c4fe4432bf002336449cf2d79
kunkka321 <kunkka321@gmail.com>
2017-06-24 20:04:30 -0500

    commmit

---
+++ app/src/main/java/pe/edu/ulima/pruebacalificada3/MainActivity.java
@@ -0,0 +1,55 @@
+package pe.edu.ulima.pruebacalificada3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.zip.Inflater;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnClickListener,OnCompetenciasCargadasListener{
+
+    ListView lviCompeticion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_competicion);
+
+
+
+
+        lviCompeticion = (ListView) findViewById(R.id.lviCompeticion);
+        CompeticionManager manager = new CompeticionManager();
+        manager.obtenerCompeticion(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onCompetenciasCargadas(List<Competencia> competencias) {
+
+
+
+        ListadoCompetenciaAdapter adapter =
+                new ListadoCompetenciaAdapter(this,competencias);
+        lviCompeticion.setAdapter(adapter);
+
+
+    }
+
+    @Override
+    public void onError(String error) {
+
+    }
+}
+
